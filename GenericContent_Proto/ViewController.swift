b2565e13c1526536758e4d332d5c0a53f713fe1e
Garrett Minky <gminky019@gmail.com>
2015-12-03 20:40:43 -0800

    Initial Commit

---
+++ GenericContent_Proto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GenericContent_Proto
+//
+//  Created by Garrett Minky on 12/3/15.
+//  Copyright © 2015 Garrett Minky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
